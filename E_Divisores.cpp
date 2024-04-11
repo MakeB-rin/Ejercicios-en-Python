7d7ea23d245d0e29575f194a990843a11440fc78
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-04-10 20:21:03 -0400

    Create E_Divisores.cpp

---
+++ E_Divisores.cpp
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(0,n):
+    a,b = map(int, input().split())
+    r = (a//b)*b
+    if(r != (a/b)*b):
+        print("La división no es exacta. Cociente:",a//b,"Resto:",a%b)
+    else:
+        print("La división es exacta. Cociente:", a // b)
