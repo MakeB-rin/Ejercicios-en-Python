f0430e31d99c55725901ae6d523bb9ae271d44dc
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-04-22 09:35:05 -0400

    Create E_SecuenciaSaltarinaV.py

---
+++ Ejercicios Normales/E_SecuenciaSaltarinaV.py
@@ -0,0 +1,16 @@
+import sys
+for multiplesCasos in sys.stdin:
+    temp = list(map(int,multiplesCasos.split()))
+    v = []
+    n = temp[0] # [1, n - 1]
+
+    sw = 1
+    for i in range(1, len(temp)):
+        v.append(temp[i])
+    for i in range(1, len(v)):
+        if(abs(v[i] - v[i - 1]) < 1) or (abs(v[i] - v[i - 1]) >= n):
+            sw = 0
+    if(sw == 1):
+        print("SI")
+    else:
+        print("NO")
