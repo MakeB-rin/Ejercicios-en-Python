3c57b96e7520e9c4f6d54d35c0764374a0a7b2d6
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-04-22 10:13:05 -0400

    Create E_JuegoDeNiños.py

---
+++ Ejercicios Normales/E_JuegoDeNiños.py
@@ -0,0 +1,11 @@
+n = int(input())
+while (n != 0):
+    v = list(map(str, input().split()))
+    v.sort()
+    v.reverse()
+
+    cad = ""
+    for i in range(0, n):
+        cad = cad + v[i]
+    print(cad)
+    n = int(input())
