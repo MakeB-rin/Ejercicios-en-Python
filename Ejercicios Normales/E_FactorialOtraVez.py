31268750d43062fc14d0a3c9cc1fc4c3e96833cd
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-04-22 10:29:49 -0400

    Create E_FactorialOtraVez.py

---
+++ Ejercicios Normales/E_FactorialOtraVez.py
@@ -0,0 +1,15 @@
+import sys
+n = sys.stdin.readline().strip()
+while(n != 0):
+    sum = 0
+    i = 1
+
+    f = 1
+    p = len(n) - 1
+    while(p >= 0):
+        f = f * i
+        i += 1
+        sum +=(ord(n[p]) - ord('0'))*f
+        p = p - 1
+    print(sum)
+    n = sys.stdin.readline().strip()
