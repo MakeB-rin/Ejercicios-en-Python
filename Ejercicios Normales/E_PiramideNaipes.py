9edd82afa636fe9c450ec14c8d26256149a27755
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-04-24 09:27:22 -0400

    Create E_PiramideNaipes.py

---
+++ Ejercicios Normales/E_PiramideNaipes.py
@@ -0,0 +1,22 @@
+def suma(n):
+    return (n*(n+1))//2
+
+def F(n):
+    return (2*(suma(n))) + suma(n-1)
+
+t = int(input())
+for casos in range(0,t):
+    n = int(input())
+    cnt = 0
+    while(n > 1):
+        cnt += 1
+        a = 0
+        b = 25821
+        while(b - a > 1):
+            med = (a + b)//2
+            if(F(med) > n):
+                b = med
+            else:
+                a = med
+        n -= F(a)
+    print(cnt)
