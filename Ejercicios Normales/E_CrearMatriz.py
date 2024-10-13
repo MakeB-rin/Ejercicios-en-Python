1671fd4b9f174e40cae06a84fd60bcd5c2f19c12
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2024-10-12 20:57:30 -0400

    Create E_CrearMatriz.py

---
+++ Ejercicios Normales/E_CrearMatriz.py
@@ -0,0 +1,14 @@
+#Cantidad Filas, Columnas
+n, m = map(int, input().split())
+#Leer una matriz de n filas y m columnas
+matriz = []
+# Llenar un matriz con datos por teclado
+
+for i in range(0, n):
+    v = list(map(int,input().split()))
+    matriz.append(v)
+
+for i in range(0 ,n):
+    for j in range(0, m):
+        print(matriz[i][j], end=" ")
+    print()
