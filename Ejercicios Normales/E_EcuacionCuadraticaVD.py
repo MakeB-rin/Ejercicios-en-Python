0d785a35b27616ea0b45b3f93c12be4d485df7b7
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2025-06-20 08:33:16 -0400

    Create E_EcuacionCuadraticaVD.py

---
+++ Ejercicios Normales/E_EcuacionCuadraticaVD.py
@@ -0,0 +1,34 @@
+import math
+a,b,c = map(float, input().split())
+d = b**2 - 4*a*c;
+
+if a == 0:
+    print("No existe solucion")
+else:
+    if(d < 0):
+        print("No existe solucion")
+    elif (d == 0):
+        r1 = -b / (2 * a)
+        print("Existe una solucion")
+        if abs(r1) < 1e-9:
+            r1 = 0.0
+        print(f"{r1:.4f}")
+    else:
+        r1 = (-b + math.sqrt(d)) / (2*a)
+        r2 = (-b - math.sqrt(d)) / (2*a)
+        if abs(r1) < 1e-9:
+            r1 = 0.0
+        if abs(r2) < 1e-9:
+            r2 = 0.0
+        print("Existen dos soluciones")
+        if(r1 < r2):
+            print(f"{r1:.4f} {r2:.4f}")
+        elif (r2 < r1):
+            print(f"{r2:.4f} {r1:.4f}")
+
+        # Ecuacion cuadratica (Version Dificil)
+#Ejemplo
+# x = -0.00000000001
+#
+# if abs(x) < 1e-9:
+#     print("Es prácticamente cero")
