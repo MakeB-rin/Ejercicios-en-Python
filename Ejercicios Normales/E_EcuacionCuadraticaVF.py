d2d05ebfe685ceca5b41392e025bb5ed66ea2787
Marcos Quispe P <84591202+MakeB-rin@users.noreply.github.com>
2025-06-20 08:32:54 -0400

    Create E_EcuacionCuadraticaVF.py

---
+++ Ejercicios Normales/E_EcuacionCuadraticaVF.py
@@ -0,0 +1,10 @@
+a, b, c = map(int, input().split())
+d = b**2 - 4*a*c;
+
+if(d < 0):
+    print("No hay solucion")
+elif (d == 0):
+    print("Existe una solucion")
+else:
+    print("Existen dos soluciones")
+    #Ecuacion cuadratica (Version Facil)
